7022520edf28309e3eb28cf14760886573508380
navnit-nextech <navnit@zotanextech.com>
2025-03-20 03:51:20 +0530

    "rem_nutri_content_modification"

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -9,7 +9,7 @@ const page = () => {
   return (
     <div>
       <ServicesHero />
-      <Servicelist/>
+      {/* <Servicelist/> */}
       <ServicesProcess />
       <ServiceQueries/>
       <GetInTouch />
