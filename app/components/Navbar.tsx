43c78887db30b3da722578fb2c23b25f96cdbe94
navnit-nextech <navnit@zotanextech.com>
2025-06-15 13:18:02 +0530

    "title logo changes"

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -69,7 +69,7 @@ const Navbar: React.FC<NavbarProps> = ({
         <div className="max-w-7xl w-full h-full mx-auto flex items-center justify-between sm:px-8">
           {/* Logo */}
           <Link href="/" className="">
-            <div className="w-50 h-50 pt-7  ">
+            <div className="w-60 h-60 pt-8  ">
               {/* SVG Logo */}
               <img src="/images/rem_nutri_logo_.png" alt="RemDi Logo" />
             </div>
